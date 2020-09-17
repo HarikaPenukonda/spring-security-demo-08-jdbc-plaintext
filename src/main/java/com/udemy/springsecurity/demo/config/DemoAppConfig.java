cc6c96f5559f14f548fd3ba1700c2a9215783c76
harika penukonda <penukondaharika@gmail.com>
2020-09-17 16:41:32 -0500

    Defining datasource in Spring Configuration

--- src/main/java/com/udemy/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/udemy/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,16 @@
 package com.udemy.springsecurity.demo.config;
 
+
+
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +18,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.udemy.springsecurity.demo")
+
 public class DemoAppConfig {
+	
+	// set up variable to hold the properties
+	
+	@Autowired
+	private Environment env; // will hold data read from properties files
+	
+	// set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	// define a bean for ViewResolver
 	
@@ -23,6 +40,25 @@ public class DemoAppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+		
+	}
+	
+	// define a bean for security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		
+		// set the jdbc driver class
+		
+		// log the connection props
+		
+		// set database connection props
+		
+		// set connection pool props
+		
+		return null;
 		
 	}
 
