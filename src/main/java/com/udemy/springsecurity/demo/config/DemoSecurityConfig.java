a83d572740efb201ec236c58c74f8493ccde4c13
harika penukonda <penukondaharika@gmail.com>
2020-09-21 09:46:29 -0500

    Update DemoSecurityConfig.java

--- src/main/java/com/udemy/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/udemy/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,22 +1,37 @@
 package com.udemy.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	// add a reference to our security data source
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// use jdbc authentication ... oh yeah!!!
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		http.authorizeRequests()
-			//.anyRequest().authenticated()
 			.antMatchers("/").hasRole("EMPLOYEE")
 			.antMatchers("/leaders/**").hasRole("MANAGER")
 			.antMatchers("/systems/**").hasRole("ADMIN")
@@ -25,28 +40,13 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
 				.permitAll()
-				.and()
-				.logout().permitAll()
-				.and()
-				.exceptionHandling().accessDeniedPage("/access-denied");
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		// add our users for in memory authentication
-		
-		@SuppressWarnings("deprecation")
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE","MANAGER"))
-			.withUser(users.username("susan").password("admin").roles("EMPLOYEE","ADMIN"));
-
-		
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
 		
 	}
+		
 	
 	
 
